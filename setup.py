88b5eb1b3620c4d0284090a9199b57d6b22ce4f5
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2023-04-16 21:03:37 -0400

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(name='indiek-mockdb',
       python_requires='>=3.8',
-      version='0.1.0',
+      version='0.1.1',
       description='mock database for indiek',
-      long_description='',
+      long_description='''This is an on-the-fly in-memory mock Database used by indiek-core
+      for development and testing purposes. The versioning of this library is locked with that
+      of indiek-core.''',
       author='Adrian Ernesto Radillo',
       author_email='adrian.radillo@gmail.com',
       license='GNU Affero General Public License v3.0',
