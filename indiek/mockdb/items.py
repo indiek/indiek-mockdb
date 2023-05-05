7cc0459ef995afc9fa990f2150cf39e51fe55fe1
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2023-05-05 18:38:27 -0400

    unittests passed

--- indiek/mockdb/items.py
+++ indiek/mockdb/items.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
-from typing import Sequence, Dict, List
+from typing import Sequence, Dict, Optional, List, Any
 
 
 def generate_id(existing: Sequence[int]) -> int:
-    """Generate ID."""  
+    """Generate ID."""
     return max(existing) + 1 if len(existing) else 0
 
 
@@ -11,7 +11,7 @@ class Item:
     """Class that represents genericItem in mockdb.
 
     In mockdb, all items are stored in the class variable `Item._item_dict`.
-    
+
     Unless specified, attributes below should be compatible
     with indiek-core.
 
@@ -23,27 +23,37 @@ class Item:
     _item_dict = {}
     """All items are stored in this class variable that maps item unique ID to their content."""
 
-    def __init__(self, name: str = '', content: str = '', ikid: int = None):
+    def __init__(
+            self, *,
+            name: Optional[str] = '',
+            content: Optional[str] = '',
+            ikid: Optional[int] = None
+    ):
         """Initialize Item.
 
         Args:
             name (str, optional): item name. Defaults to ''.
             content (str, optional): item description. Defaults to ''.
-            ikid (int, optional): item ID. Defaults to None.
+            ikid (int, optional): item id in mockdb; note that if the id corresponds to an existing
+                item in the DB and a save operation occurs later on, the existing data will be overriden.
         """
+        self.ikid = ikid
         self.name = name
         self.content = content
-        self.ikid = ikid
 
-    def save(self):
+    def save(self) -> int:
         """Save instance data into mockdb.
-        
+
         If the instance doesn't have an id, (`ikid` attr), a new unique one
         is generated add added before saving.
+
+        Returns:
+            int: ikid of item
         """
         if self.ikid is None:
             self.ikid = generate_id(self._item_dict.keys())
         self._item_dict[self.ikid] = self.to_dict()
+        return self.ikid
 
     def to_dict(self):
         """Return mockdb Item instance content as dict.
@@ -53,9 +63,9 @@ class Item:
         """
         return {
             'ikid': self.ikid,
-            'name': self.name, 
+            'name': self.name,
             'content': self.content
-            }
+        }
 
     @staticmethod
     def from_core(pure_item: Any) -> Item:
@@ -81,7 +91,7 @@ class Item:
         """
         dict_ = Item._item_dict[ikid]
         return Item(**dict_)
-    
+
     @classmethod
     def list_all(cls) -> List[Dict]:
         """Expose mockdb items as list of dict.
--- tests/test_items.py
+++ tests/test_items.py
@@ -4,6 +4,8 @@ from indiek.mockdb.items import Item
 
 class TestItemAttr(unittest.TestCase):
     def test_instantiation(self):
+        """Test that mockdb Item has the attr specified by indiek-core API."""
+
         item = Item()
         expected_attr = [
             'name',
@@ -12,6 +14,30 @@ class TestItemAttr(unittest.TestCase):
         for attr_name in expected_attr:
             self.assertTrue(hasattr(item, attr_name))
 
+class TestItemIO(unittest.TestCase):
+    def test_write_read(self):
+        item1_dict = {'name': 'item1', 'content': 'blabla'}
+        item1 = Item(**item1_dict)
+        item1_dict['ikid'] = item1.save()
+
+        item2_dict = dict(name='item2', content='nslkdf')
+        item2 = Item(**item2_dict)
+        item2_dict['ikid'] = item2.save()
+        del item1, item2
+
+        item1 = Item.load(item1_dict['ikid'])
+        self.assertDictEqual(item1.to_dict(), item1_dict)
+
+        item2 = Item.load(item2_dict['ikid'])
+        self.assertDictEqual(item2.to_dict(), item2_dict)
+
+    def test_list_all(self):
+        item3_dict = {'name': 'item1', 'content': 'blabla'}
+        item3 = Item(**item3_dict)
+        item3_dict['ikid'] = item3.save()
+
+        stored = Item.list_all()
+        self.assertIn(item3_dict, stored)
 
 if __name__ == '__main__':
     unittest.main()
