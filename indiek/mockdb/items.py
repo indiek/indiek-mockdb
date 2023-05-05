9780dea7e3e4fd3e01c43cd6ad8b9d52dd9145b4
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2023-05-05 18:12:19 -0400

    improve docstrings in items module

--- indiek/mockdb/items.py
+++ indiek/mockdb/items.py
@@ -1,26 +1,92 @@
-def generate_id(existing):    
+from __future__ import annotations
+from typing import Sequence, Dict, List
+
+
+def generate_id(existing: Sequence[int]) -> int:
+    """Generate ID."""  
     return max(existing) + 1 if len(existing) else 0
 
+
 class Item:
+    """Class that represents genericItem in mockdb.
+
+    In mockdb, all items are stored in the class variable `Item._item_dict`.
+    
+    Unless specified, attributes below should be compatible
+    with indiek-core.
+
+    Attributes:
+        name (str): item name.
+        content (str): item content.
+        ikid (int): ID of item in mockdb. This is not part of indiek-core API.
+    """
     _item_dict = {}
+    """All items are stored in this class variable that maps item unique ID to their content."""
+
     def __init__(self, name: str = '', content: str = '', ikid: int = None):
+        """Initialize Item.
+
+        Args:
+            name (str, optional): item name. Defaults to ''.
+            content (str, optional): item description. Defaults to ''.
+            ikid (int, optional): item ID. Defaults to None.
+        """
         self.name = name
         self.content = content
         self.ikid = ikid
 
     def save(self):
+        """Save instance data into mockdb.
+        
+        If the instance doesn't have an id, (`ikid` attr), a new unique one
+        is generated add added before saving.
+        """
         if self.ikid is None:
             self.ikid = generate_id(self._item_dict.keys())
         self._item_dict[self.ikid] = self.to_dict()
 
     def to_dict(self):
-        return {'name': self.name, 'content': self.content}
+        """Return mockdb Item instance content as dict.
+
+        Returns:
+            dict: mockdb Item instance
+        """
+        return {
+            'ikid': self.ikid,
+            'name': self.name, 
+            'content': self.content
+            }
 
     @staticmethod
-    def from_core(pure_item):
+    def from_core(pure_item: Any) -> Item:
+        """Instantiate mockdb Item off of an Item from indiek-core.
+
+        Args:
+            pure_item (Any): item instance from indiek-core library.
+
+        Returns:
+            Item: mockdb Item instance
+        """
         return Item(**pure_item.to_dict())
 
     @staticmethod
-    def load(ikid):
+    def load(ikid: int) -> Item:
+        """Load (instantiate) mockdb Item from its ID.
+
+        Args:
+            ikid (int): item ID in mockdb.
+
+        Returns:
+            Item: mockdb Item instance
+        """
         dict_ = Item._item_dict[ikid]
         return Item(**dict_)
+    
+    @classmethod
+    def list_all(cls) -> List[Dict]:
+        """Expose mockdb items as list of dict.
+
+        Returns:
+            List[Dict]: the values from Item._item_dict class attr.
+        """
+        return list(cls._item_dict.values())
