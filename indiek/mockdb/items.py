ccd6358d7d823c14cdca36029424aeec1e351b99
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2023-05-06 09:47:16 -0400

    unittests passed

--- indiek/mockdb/items.py
+++ indiek/mockdb/items.py
@@ -18,26 +18,42 @@ class Item:
     Attributes:
         name (str): item name.
         content (str): item content.
-        ikid (int): ID of item in mockdb. This is not part of indiek-core API.
+        _ikid (int): ID of item in mockdb. This is not part of indiek-core API.
     """
+
     _item_dict = {}
     """All items are stored in this class variable that maps item unique ID to their content."""
 
+    def __repr__(self):
+        _ikid = self._ikid
+        name = self.name
+        content_hash = hash(self.content)
+        return f"MockDB Item:{_ikid=};{name=};{content_hash=}"
+    
+    def __str__(self):
+        return f"MockDB Item with ID {self._ikid} and name {self.name}"
+
+    def __eq__(self, other) -> bool:
+        cond = self._ikid == other._ikid
+        cond = cond and self.name == other.name
+        cond = cond and self.content == other.content
+        return cond and isinstance(other, self.__class__)
+
     def __init__(
             self, *,
-            name: Optional[str] = '',
-            content: Optional[str] = '',
-            ikid: Optional[int] = None
+            name: str = '',
+            content: str = '',
+            _ikid: Optional[int] = None
     ):
         """Initialize Item.
 
         Args:
             name (str, optional): item name. Defaults to ''.
             content (str, optional): item description. Defaults to ''.
-            ikid (int, optional): item id in mockdb; note that if the id corresponds to an existing
+            _ikid (int, optional): item id in mockdb; note that if the id corresponds to an existing
                 item in the DB and a save operation occurs later on, the existing data will be overriden.
         """
-        self.ikid = ikid
+        self._ikid = _ikid
         self.name = name
         self.content = content
 
@@ -48,12 +64,12 @@ class Item:
         is generated add added before saving.
 
         Returns:
-            int: ikid of item
+            int: _ikid of item
         """
-        if self.ikid is None:
-            self.ikid = generate_id(self._item_dict.keys())
-        self._item_dict[self.ikid] = self.to_dict()
-        return self.ikid
+        if self._ikid is None:
+            self._ikid = generate_id(self._item_dict.keys())
+        self._item_dict[self._ikid] = self.to_dict()
+        return self._ikid
 
     def to_dict(self):
         """Return mockdb Item instance content as dict.
@@ -62,7 +78,7 @@ class Item:
             dict: mockdb Item instance
         """
         return {
-            'ikid': self.ikid,
+            '_ikid': self._ikid,
             'name': self.name,
             'content': self.content
         }
@@ -80,7 +96,7 @@ class Item:
         return Item(**pure_item.to_dict())
 
     @staticmethod
-    def load(ikid: int) -> Item:
+    def load(_ikid: int) -> Item:
         """Load (instantiate) mockdb Item from its ID.
 
         Args:
@@ -89,14 +105,14 @@ class Item:
         Returns:
             Item: mockdb Item instance
         """
-        dict_ = Item._item_dict[ikid]
+        dict_ = Item._item_dict[_ikid]
         return Item(**dict_)
 
     @classmethod
-    def list_all(cls) -> List[Dict]:
-        """Expose mockdb items as list of dict.
+    def list_all(cls) -> List[Item]:
+        """Fetch all stored mockdb items as a list.
 
         Returns:
-            List[Dict]: the values from Item._item_dict class attr.
+            List[Item]: stored items.
         """
-        return list(cls._item_dict.values())
+        return [cls(**item_dict) for item_dict in cls._item_dict.values()]
--- tests/test_items.py
+++ tests/test_items.py
@@ -10,6 +10,8 @@ class TestItemAttr(unittest.TestCase):
         expected_attr = [
             'name',
             'content',
+            '_ikid',
+            'to_dict'
         ]
         for attr_name in expected_attr:
             self.assertTrue(hasattr(item, attr_name))
@@ -18,26 +20,26 @@ class TestItemIO(unittest.TestCase):
     def test_write_read(self):
         item1_dict = {'name': 'item1', 'content': 'blabla'}
         item1 = Item(**item1_dict)
-        item1_dict['ikid'] = item1.save()
+        item1_dict['_ikid'] = item1.save()
 
         item2_dict = dict(name='item2', content='nslkdf')
         item2 = Item(**item2_dict)
-        item2_dict['ikid'] = item2.save()
+        item2_dict['_ikid'] = item2.save()
         del item1, item2
 
-        item1 = Item.load(item1_dict['ikid'])
+        item1 = Item.load(item1_dict['_ikid'])
         self.assertDictEqual(item1.to_dict(), item1_dict)
 
-        item2 = Item.load(item2_dict['ikid'])
+        item2 = Item.load(item2_dict['_ikid'])
         self.assertDictEqual(item2.to_dict(), item2_dict)
 
     def test_list_all(self):
         item3_dict = {'name': 'item1', 'content': 'blabla'}
         item3 = Item(**item3_dict)
-        item3_dict['ikid'] = item3.save()
+        item3.save()
 
         stored = Item.list_all()
-        self.assertIn(item3_dict, stored)
+        self.assertIn(item3, stored)
 
 if __name__ == '__main__':
     unittest.main()
